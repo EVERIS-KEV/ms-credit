9f0a1baea48a48fd1a3fcad50c59687301c8662f
Mauricio06Everis <kpalmall@everis.com>
2021-08-22 16:39:49 -0500

    Update 1

--- src/main/java/com/everis/credit/controller/creditController.java
+++ src/main/java/com/everis/credit/controller/creditController.java
@@ -1,80 +1,76 @@
 package com.everis.credit.controller;
 
-import com.everis.credit.dto.*;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.everis.credit.dto.AuthFrom;
+import com.everis.credit.dto.creditFrom;
+import com.everis.credit.dto.message;
 import com.everis.credit.model.credit;
 import com.everis.credit.service.creditService;
 
-import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
-@CrossOrigin(
-  origins = "*",
-  methods = {
-    RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE
-  }
-)
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+		RequestMethod.DELETE })
 @RequestMapping()
 public class creditController {
-  @Autowired
-  creditService service;
-  
-  @GetMapping("/")
-  public Flux<credit> findAll(){
-	  return service.getAll();
-  }
+	@Autowired
+	creditService service;
 
-  @GetMapping("/byCustomer/{id}")
-  public Flux<credit> getById(@PathVariable("id") String id) {
-    return service.getByCustomer(id);
-  }
-  
-  @GetMapping("/verifyCustomer/{id}")
-  public Mono<Boolean> verify(@PathVariable("id") String id) {
-	  return service._verifyCustomer(id);
-  }
+	@GetMapping("/")
+	public Flux<credit> findAll() {
+		return service.getAll();
+	}
 
-  @PostMapping("/save")
-  public Mono<Object> created(
-    @RequestBody @Valid creditFrom model,
-    BindingResult bindinResult
-  ) {
-    String msg = "";
+	@GetMapping("/byCustomer/{id}")
+	public Flux<credit> getById(@PathVariable("id") String id) {
+		return service.getByCustomer(id);
+	}
 
-    if (bindinResult.hasErrors()) {
-      for (int i = 0; i < bindinResult.getAllErrors().size(); i++) msg =
-        bindinResult.getAllErrors().get(0).getDefaultMessage();
-      return Mono.just(new message(msg));
-    }
+	@GetMapping("/verifyCustomer/{id}")
+	public Mono<Boolean> verify(@PathVariable("id") String id) {
+		return service._verifyCustomer(id);
+	}
 
-    return service.save(
-      model.getIdCustomer(),
-      model.getBaseCreditLimit(),
-      model.getPassword()
-    );
-  }
+	@PostMapping("/save")
+	public Mono<Object> created(@RequestBody @Valid creditFrom model, BindingResult bindinResult) {
+		String msg = "";
 
-  @PostMapping("/operations")
-  public Mono<Object> consumptions(
-    @RequestBody @Valid AuthFrom model,
-    BindingResult bindinResult
-  ) {
-    String msg = "";
+		if (bindinResult.hasErrors()) {
+			for (int i = 0; i < bindinResult.getAllErrors().size(); i++) {
+				msg = bindinResult.getAllErrors().get(0).getDefaultMessage();
+			}
+			return Mono.just(new message(msg));
+		}
 
-    if (bindinResult.hasErrors()) {
-      for (int i = 0; i < bindinResult.getAllErrors().size(); i++) msg =
-        bindinResult.getAllErrors().get(0).getDefaultMessage();
-      return Mono.just(new message(msg));
-    }
+		return service.save(model.getIdCustomer(), model.getBaseCreditLimit(), model.getPassword());
+	}
 
-    return service.saveOperations(
-      model.getCreditCardNumber(),
-      model.getPassword(),
-      model.getAmount(),
-      model.getType()
-    );
-  }
+	@PostMapping("/operations")
+	public Mono<Object> consumptions(@RequestBody @Valid AuthFrom model, BindingResult bindinResult) {
+		String msg = "";
+
+		if (bindinResult.hasErrors()) {
+			for (int i = 0; i < bindinResult.getAllErrors().size(); i++) {
+				msg = bindinResult.getAllErrors().get(0).getDefaultMessage();
+			}
+			return Mono.just(new message(msg));
+		}
+
+		return service.saveOperations(model.getCreditCardNumber(), model.getPassword(), model.getAmount(),
+				model.getType());
+	}
 }
--- src/main/java/com/everis/credit/webclient/webclient.java
+++ src/main/java/com/everis/credit/webclient/webclient.java
@@ -4,9 +4,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 public class webclient {
   public static WebClient customer = WebClient.create(
-    "http://host.docker.internal:8090/service/customers"
+    "http://44.196.6.42:8090/service/customers"
   );
   public static WebClient logic = WebClient.create(
-    "http://host.docker.internal:8090/service/logic"
+    "http://44.196.6.42:8090/service/logic"
   );
 }
